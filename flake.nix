5461e0eb214d0ad91f611dd3e63e02ae8007d257
drzzln <drzzln@protonmail.com>
2026-02-27 21:11:04 -0500

    chore: switch nixpkgs to nixos-25.11 stable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   description = "Observability library — tracing, metrics, distributed tracing";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     substrate = {
       url = "github:pleme-io/substrate";
       inputs.nixpkgs.follows = "nixpkgs";
